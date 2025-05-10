ea7d9d56e1da59b9c899922e811b1c7656ae3f42
Huy Huy <nhathuych@gmail.com>
2025-05-10 22:36:54 +0700

    add transaction APIs with Stripe integration

---
+++ src/routes/transactionRoutes.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import { createStripePaymentIntent, createTransaction } from '../controllers/transactionController'
+
+const router = express.Router()
+
+router.post('/', createTransaction)
+router.post('/stripe/payment-intent', createStripePaymentIntent)
+
+export default router
